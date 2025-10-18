4982a330f6610fafe2eeb5865f202cc51ffc8247
lizard_demon <iamjustafreak41@gmail.com>
2025-10-17 22:36:41 -0400

    Import dearimgui

--- build.zig
+++ build.zig
@@ -1,17 +1,28 @@
 const std = @import("std");
 const Build = std.Build;
 const sokol = @import("sokol");
+const cimgui = @import("cimgui");
 
-const Opts = struct { mod: *Build.Module, dep_sokol: *Build.Dependency, shader: *Build.Step };
+const Opts = struct { mod: *Build.Module, dep_sokol: *Build.Dependency, dep_cimgui: *Build.Dependency, cimgui_clib_name: []const u8, shader: *Build.Step };
 
 pub fn build(b: *Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-    const dep_sokol = b.dependency("sokol", .{ .target = target, .optimize = optimize });
+    const opt_docking = b.option(bool, "docking", "Build with docking support") orelse false;
+
+    const cimgui_conf = cimgui.getConfig(opt_docking);
+    const dep_sokol = b.dependency("sokol", .{ .target = target, .optimize = optimize, .with_sokol_imgui = true });
+    const dep_cimgui = b.dependency("cimgui", .{ .target = target, .optimize = optimize });
     const shdc = b.dependency("shdc", .{});
+
+    dep_sokol.artifact("sokol_clib").addIncludePath(dep_cimgui.path(cimgui_conf.include_dir));
+
     const shader = try @import("shdc").createSourceFile(b, .{ .shdc_dep = shdc, .input = "src/shaders/cube.glsl", .output = "src/shaders/cube.glsl.zig", .slang = .{ .glsl410 = true, .glsl300es = true, .metal_macos = true, .wgsl = true } });
-    const mod = b.createModule(.{ .root_source_file = b.path("src/main.zig"), .target = target, .optimize = optimize, .imports = &.{.{ .name = "sokol", .module = dep_sokol.module("sokol") }} });
-    const opts = Opts{ .mod = mod, .dep_sokol = dep_sokol, .shader = shader };
+    const mod = b.createModule(.{ .root_source_file = b.path("src/main.zig"), .target = target, .optimize = optimize, .imports = &.{
+        .{ .name = "sokol", .module = dep_sokol.module("sokol") },
+        .{ .name = cimgui_conf.module_name, .module = dep_cimgui.module(cimgui_conf.module_name) },
+    } });
+    const opts = Opts{ .mod = mod, .dep_sokol = dep_sokol, .dep_cimgui = dep_cimgui, .cimgui_clib_name = cimgui_conf.clib_name, .shader = shader };
     if (target.result.cpu.arch.isWasm()) try buildWeb(b, opts) else try buildNative(b, opts);
 }
 
@@ -28,6 +39,11 @@ fn buildWeb(b: *Build, opts: Opts) !void {
     const lib = b.addLibrary(.{ .name = "voxels", .root_module = opts.mod });
     lib.step.dependOn(opts.shader);
     const emsdk = opts.dep_sokol.builder.dependency("emsdk", .{});
+
+    const emsdk_incl_path = emsdk.path("upstream/emscripten/cache/sysroot/include");
+    opts.dep_cimgui.artifact(opts.cimgui_clib_name).addSystemIncludePath(emsdk_incl_path);
+    opts.dep_cimgui.artifact(opts.cimgui_clib_name).step.dependOn(&opts.dep_sokol.artifact("sokol_clib").step);
+
     const link_step = try sokol.emLinkStep(b, .{ .lib_main = lib, .target = opts.mod.resolved_target.?, .optimize = opts.mod.optimize.?, .emsdk = emsdk, .use_webgl2 = true, .use_emmalloc = true, .use_filesystem = false, .shell_file_path = opts.dep_sokol.path("src/sokol/web/shell.html") });
     b.getInstallStep().dependOn(&link_step.step);
     const run = sokol.emRunStep(b, .{ .name = "voxels", .emsdk = emsdk });
